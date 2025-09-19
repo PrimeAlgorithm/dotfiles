60ea6523e8816af3d64cc0ef6365e486dc9d2723
PrimeAlgorithm <219721209+PrimeAlgorithm@users.noreply.github.com>
2025-09-18 18:04:58 -0700

    first commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Font
+config.font = wezterm.font("JetBrains Mono Regular")
+config.font_size = 12.5
+
+-- Colors
+config.color_scheme = "Tokyo Night"
+
+-- Window
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+config.window_decorations = "TITLE | RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Misc
+config.default_prog = { 'powershell' }
+
+return config
